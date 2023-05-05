b1eaa505db507e8a0ab00511c6e5b776aec40048
tech-911 <babatundeolatunji74@gmail.com>
2023-05-05 22:39:37 +0100

    ...completed basic dev of nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.scss";
 import { Link } from "react-router-dom";
 import {
@@ -7,9 +7,13 @@ import {
   AiFillLinkedin,
 } from "react-icons/ai";
 import { HiOutlineShoppingBag, HiOutlineSearch } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrFormClose, GrFacebookOption } from "react-icons/gr";
 const Nav = () => {
+  const [navDrop, setNavDrop] = useState(0);
+  console.log(navDrop);
   return (
-    <div className="nav_wrapper">
+    <div className={`nav_wrapper ${navDrop ? "nav_wrapper_white" : ""}`}>
       <div className="nav_left">
         <h1 className="nav_name">DAVIES K. BAMIGBOYE</h1>
         <AiOutlineTwitter className="nav_twitter" />
@@ -17,15 +21,96 @@ const Nav = () => {
         <AiFillLinkedin className="nav_linkden" />
       </div>
       <div className="nav_right">
-        <Link className="nav_link1 nav_link">Home</Link>
-        <Link className="nav_link2 nav_link">Start Here</Link>
-        <Link className="nav_link3 nav_link">Courses</Link>
-        <Link className="nav_link4 nav_link">Blog</Link>
-        <Link className="nav_link5 nav_link">Shop</Link>
-        <Link className="nav_link6 nav_link">Contact</Link>
+        <Link to="/" className="nav_link1 nav_link">
+          Home
+        </Link>
+        <Link to="/" className="nav_link2 nav_link">
+          Start Here
+        </Link>
+        <Link to="/" className="nav_link3 nav_link">
+          Courses
+        </Link>
+        <Link to="/" className="nav_link4 nav_link">
+          Blog
+        </Link>
+        <Link to="/" className="nav_link5 nav_link">
+          Shop
+        </Link>
+        <Link to="/" className="nav_link6 nav_link">
+          Contact
+        </Link>
         <div className="nav_aux_wrap">
           <HiOutlineShoppingBag className="nav_shop" />
           <HiOutlineSearch className="nav_search" />
+        </div>
+      </div>
+      <div className="navres_wrap">
+        <div className="navres_left">
+          {navDrop ? (
+            <GrFormClose
+              onClick={() => {
+                setNavDrop(!navDrop);
+              }}
+              className={`navres_right ${
+                navDrop ? "navres_element_black" : ""
+              }`}
+            />
+          ) : (
+            <GiHamburgerMenu
+              onClick={() => {
+                setNavDrop(!navDrop);
+              }}
+              className={`navres_left_icon ${
+                navDrop ? "navres_element_black" : ""
+              }`}
+            />
+          )}
+
+          <p
+            className={`navres_left_text ${
+              navDrop ? "navres_element_black" : ""
+            }`}
+          >
+            DAVIES K. BAMIGBOYE
+          </p>
+        </div>
+        <HiOutlineShoppingBag
+          className={`navres_right ${navDrop ? "navres_element_black" : ""}`}
+        />
+      </div>
+      <div
+        className={`navres_drop_wrap ${
+          navDrop ? "navres_drop_wrap_open" : "navres_drop_wrap_close"
+        }`}
+      >
+        <Link to="/" className="navres_link1 navres_link">
+          Home
+        </Link>
+        <div className="navres_border"></div>
+        <Link to="/" className="navres_link2 navres_link">
+          Start Here
+        </Link>
+        <div className="navres_border"></div>
+        <Link to="/" className="navres_link3 navres_link">
+          Courses
+        </Link>
+        <div className="navres_border"></div>
+        <Link to="/" className="navres_link4 navres_link">
+          Blog
+        </Link>
+        <div className="navres_border"></div>
+        <Link to="/" className="navres_link5 navres_link">
+          Shop
+        </Link>
+        <div className="navres_border"></div>
+        <Link to="/" className="navres_link6 navres_link">
+          Contact
+        </Link>
+        <div className="navres_aux_wrap">
+          <GrFacebookOption className="navres_facebook" />
+          <AiOutlineTwitter className="navres_twitter" />
+          <AiOutlineInstagram className="navres_instagram" />
+          <AiFillLinkedin className="navres_linkden" />
         </div>
       </div>
     </div>
