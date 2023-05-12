32772208cc6d2d7813930818fec86ea0752b6e3f
tech-911 <babatundeolatunji74@gmail.com>
2023-05-12 22:57:03 +0100

    ...updating

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./blog.scss";
 import BlogHeader from "../../components/blog_header/BlogHeader";
+import Footer from "../../components/footer/Footer";
 
 const Blog = () => {
   return (
     <div className="blog_wrapper">
       <BlogHeader />
+      <Footer />
     </div>
   );
 };
